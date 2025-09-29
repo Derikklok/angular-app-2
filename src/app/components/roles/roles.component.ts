34dd59b182f8c2b113213e067a2e62fcc4e6e87a
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-29 17:45:16 +0530

    Enhance Roles component with Bootstrap styling and functionality, including email input and welcome toast alert. Update package.json and package-lock.json to include @types/bootstrap.

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Toast } from 'bootstrap';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
   employeeName: string = "Sachin";
   createdAt:Date = new Date();
-
+  private toastInstance?: Toast;
+  
+  constructor(private hostElementRef: ElementRef<HTMLElement>) {}
+  
+  showWelcomeAlert(){
+    if (!this.toastInstance) {
+      const toastElement = this.hostElementRef.nativeElement.querySelector('#welcomeToast');
+      if (toastElement) {
+        this.toastInstance = new Toast(toastElement, { delay: 2500 });
+      }
+    }
+    this.toastInstance?.show();
+  }
 }
