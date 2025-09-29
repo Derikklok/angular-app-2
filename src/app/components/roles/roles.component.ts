0a5fff021e0f5ddbba3f6320eeb1cad671d9960b
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-29 16:43:35 +0530

    Add Bootstrap integration and create Designation and Roles components

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  employeeName: string = "Sachin";
+  createdAt:Date = new Date();
+
+}
