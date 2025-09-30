d35f9b77a9797103cb459674a123daae0461b019
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-30 12:00:19 +0530

    Implement role management features by adding a proxy configuration for API calls, enhancing the Roles component to fetch and display roles from the backend, and updating the app configuration to include the HttpClient module. Additionally, introduce a new interface for role data structure.

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,23 +1,40 @@
-import { Component, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Toast } from 'bootstrap';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
-export class RolesComponent {
-  employeeName: string = "Sachin";
-  createdAt:Date = new Date();
+export class RolesComponent implements OnInit {
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
+
+  getAllRoles() {
+    this.http.get('/api/ClientStrive/GetAllRoles').subscribe((res: any) => {
+      this.roleList = res.data;
+    });
+  }
+
+  employeeName: string = 'Sachin';
+  createdAt: Date = new Date();
   private toastInstance?: Toast;
-  
+
   constructor(private hostElementRef: ElementRef<HTMLElement>) {}
-  
-  showWelcomeAlert(){
+
+  showWelcomeAlert() {
     if (!this.toastInstance) {
-      const toastElement = this.hostElementRef.nativeElement.querySelector('#welcomeToast');
+      const toastElement =
+        this.hostElementRef.nativeElement.querySelector('#welcomeToast');
       if (toastElement) {
         this.toastInstance = new Toast(toastElement, { delay: 2500 });
       }
